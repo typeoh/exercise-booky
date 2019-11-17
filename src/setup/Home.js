862871f131b2a3560f06e958a0c38e81ba63b6e3
Shauna Griffin <shauna@Shaunas-MBP.fritz.box>
2019-11-17 17:22:08 +0100

    Fix export error

--- src/setup/Home.js
+++ src/setup/Home.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {files} from "../utils/generatePagination";
+import { pages } from "../utils/generatePagination";
 
 export function Home() {
   return (
     <div>
       <h1 style={{ textAlign: "center" }}>React Components Training 1</h1>
       <div>
-        {console.log(files)}
-        {files.map(({ filename }) => {
+        {console.log(pages)}
+        {pages.map(({ filename }) => {
           return (
             <div key={filename} style={{ margin: 10 }}>
               <Link to={`/react/${filename}`}>Training {filename}</Link>
